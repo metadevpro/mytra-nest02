9ebbeaa87f2c7b293ee521ce9212d0a0e1ed294e
Pedro J. Molina <pjmolina@gmail.com>
2023-03-08 15:51:05 +0100

    added openapi

--- apps/nx02/src/main.ts
+++ apps/nx02/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
